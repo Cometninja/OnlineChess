fb28cfffc1aa311e9d37ffa8e667ca43b5233bfa
Daniel Casey <daniel_mn_casey@hotmail.com>
2023-07-20 14:08:57 +0100

    added in chat dialoge and file read write functions

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,45 @@
+function writeToFile() {
+    const content = document.getElementById('content').value;
+
+    fetch('/writeToFile', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ content }),
+    })
+        .then((response) => response.text())
+        .then((message) => {
+            alert(message);
+        })
+        .catch((error) => {
+            console.error('Error writing to file:', error);
+            alert('Error Writing to File!!');
+        });
+    receiveData();
+};
+
+function receiveData() {
+    fetch("/readfile")
+        .then((response) => response.text())
+        .then((content) => {
+            document.getElementById("display").innerText = content;
+        })
+        .catch((error) => {
+            console.error('Error Reading File!!', error);
+            alert('Error Reading File!!!');
+        });
+}
+
 function thisfunction() {
     console.log("hi i am in the file");
+}
+
+function checkmoves() {
+
+}
+
+function doSomeThing() {
+    console.log("i am trying something over here!!!");
+
 }
--- server.js
+++ server.js
@@ -1,17 +1,50 @@
 var http = require('http');
 var fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
 const { connect } = require('http2');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
+app.post('/writeToFile', (req, res) => {
+    const content = req.body.content;
 
-app.listen(3000, () => {
+    const filePath = 'data.txt';
+
+    fs.appendFile(filePath, content + '\n', (err) => {
+        if (err) {
+            console.error('Error writing to file:', err);
+            res.status(500).send('Error Writing to file');
+        } else {
+            console.log('Content appended to file sucessfully!');
+            res.status(200).send('Content appended to file sucessfully!!');
+        }
+    });
+});
+
+app.get("/readfile", (req, res) => {
+    fs.readFile('data.txt', 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Error Reading File', err);
+            res.status(500).send('Error Reading File!!');
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.listen(3000, '0.0.0.0', () => {
     console.log("Server is running on 3000");
 });
 
 app.on('connection', function (client) {
     console.log("something");
-})
+});
+
+function WriteToFile() {
+    fs.writeFile("./testing.txt", "hello i am in a file");
+}
